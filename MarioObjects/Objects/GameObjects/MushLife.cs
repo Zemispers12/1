00630f76b1ff89172731c23e679cd0f1bc2c53f6
Zemispers12 <zemisper@yandex.ru>
2025-02-16 18:15:18 +0300

    Add files via upload

---
+++ MarioObjects/Objects/GameObjects/MushLife.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MarioObjects.Objects.BaseObjects;
+
+namespace MarioObjects.Objects.GameObjects
+{
+    public class MushLife : MoveableAnimatedObject
+    {
+        public override void OnWalk(object sender, EventArgs e)
+        {
+            if (Live)
+                base.OnWalk(sender, e);
+        }
+
+        public MushLife(int x, int y)
+            : base(ObjectType.OT_MushLife)
+        {
+            ImageCount = 2;
+            ImageIndex = 1;
+            this.x = x;
+            this.y = y;
+            WalkStep = 2;
+            SetWidthHeight();
+            Live = false;
+            Visible = false;
+
+            TimerGenerator.AddTimerEventHandler(TimerType.TT_50, OnWalk);
+        }
+    }
+
+   
+}
